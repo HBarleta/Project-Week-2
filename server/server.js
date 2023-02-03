29cb3c8e3fea2f2afb5db8304656564b6691d57a
Harold Barleta <hbf20c@gmail.com>
2023-02-03 11:50:42 -0800

    fixed server

--- server/server.js
+++ server/server.js
@@ -10,4 +10,4 @@ app.use(cors())
 const Routes = require('./routes/cafe.route')
 Routes(app);
 
-app.listen(process.env.PORT || 8000, () => console.log(`You are connected to Cafe server at port ${port}`))
+app.listen(process.env.PORT || 8000, () => console.log(`You are connected to Cafe server`))
